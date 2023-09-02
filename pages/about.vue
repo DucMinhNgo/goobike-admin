47ec025003d0bd0beef85d297e0b3e44e8ffcc6e
ducngo123456 <135568147+duc-ngo-saltovn@users.noreply.github.com>
2023-09-02 20:16:24 +0700

    server route

--- pages/about.vue
+++ pages/about.vue
@@ -1,20 +1,28 @@
 <template>
-    <div>
-        <h2>About</h2>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore, incidunt laboriosam? Placeat cupiditate architecto iusto laborum maiores ipsam laudantium? Pariatur?</p>
-    </div>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore, incidunt
+      laboriosam? Placeat cupiditate architecto iusto laborum maiores ipsam
+      laudantium? Pariatur?
+    </p>
+    <p>{{ data }}</p>
+  </div>
 </template>
 
 <script setup>
-
+const { data } = await useFetch("/api/ninja?name=mario", {
+  method: "post",
+  body: { age: 25 },
+});
 </script>
 
 <style lang="scss" scoped>
-    h2 {
-        margin-bottom: 20px;
-        font-size: 36px
-    }
-    p {
-        margin: 20px 0;
-    }
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
 </style>
