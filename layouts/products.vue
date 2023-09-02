855428a6a62e6e798f2ca567ef37d66edb52d5d6
ducngo123456 <135568147+duc-ngo-saltovn@users.noreply.github.com>
2023-09-02 10:59:16 +0700

    tailwindcss

--- layouts/products.vue
+++ layouts/products.vue
@@ -1,22 +1,22 @@
 <template>
     <div>
-        <header>
-            <NuxtLink to="/">Nuxt Dojo</NuxtLink>
+        <header class="shadown-sm bg-white">
+            <nav class="container mx-auto p-4">
+                <NuxtLink to="/" class="font-bold">Nuxt Dojo</NuxtLink>
+            </nav>
         </header>
 
         <!-- Output the page content -->
-        <div>
+        <div class="container mx-auto p-4">
             <slot />
         </div>
 
-        <footer>
-            <nav>
-                <ul>
+        <footer class="container mx-auto p-4 flex justify-between border-t-2">
+                <ul class="flex gap-4">
                     <li><NuxtLink to="/">Home</NuxtLink></li>
                     <li><NuxtLink to="/about">About</NuxtLink></li>
                     <li><NuxtLink to="/products">Products</NuxtLink></li>
                 </ul>
-            </nav>
         </footer>
     </div>
 </template>
