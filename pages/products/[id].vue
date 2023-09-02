b5af4fa74f1816f5ddd477d74c9a062a558d16d9
ducngo123456 <135568147+duc-ngo-saltovn@users.noreply.github.com>
2023-09-02 18:53:46 +0700

    reuse components

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,21 +1,17 @@
 <template>
-    <div>
-        <p>Product detail for {{ id }}</p>
-        <p>{{ product.title }}</p>
-        <p>{{ product.price }}</p>
-        <p>{{ product.id }}</p>
-    </div>
+  <div>
+    <ProductDetails :product="product" />
+  </div>
 </template>
 
 <script setup>
-    const { id } = useRoute().params;
-    definePageMeta({
-        layout: 'products'
-    })
-    const uri = `https://fakestoreapi.com/products/${id}`;
-    const { data: product } = await useFetch(uri, { key: id})
+const { id } = useRoute().params;
+definePageMeta({
+  layout: "products",
+});
+const uri = `https://fakestoreapi.com/products/${id}`;
+const { data: product } = await useFetch(uri, { key: id });
 </script>
 
 <style lang="scss" scoped>
-
 </style>
